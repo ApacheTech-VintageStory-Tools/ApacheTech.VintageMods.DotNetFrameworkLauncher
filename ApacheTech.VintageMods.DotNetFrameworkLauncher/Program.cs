a153e6e6774a2a869edc198af883e89ee19c662f
ApacheTech Solutions <p_matthews@live.co.uk>
2021-10-20 04:54:13 +0100

    Add project files.

---
+++ ApacheTech.VintageMods.DotNetFrameworkLauncher/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ApacheTech.VintageMods.DotNetFrameworkLauncher
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+#if DEBUG 
+#else
+            System.Console.ForegroundColor = System.ConsoleColor.Red;
+            System.Console.WriteLine("Launcher run in Release configuration. Please change to Debug configuration, and try again.");
+            System.Console.ReadKey();
+#endif
+        }
+    }
+}
